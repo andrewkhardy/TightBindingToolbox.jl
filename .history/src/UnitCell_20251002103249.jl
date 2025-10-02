f441418a51bec76185ce87e3535b64594c741db6
Andrew Hardy <andrewkoehlerhardy@gmail.com>
2025-10-02 10:33:47 -0400

    removing rank as required territory

--- .history/src/UnitCell_20251002103249.jl
+++ .history/src/UnitCell_20251002103249.jl
@@ -117,7 +117,7 @@ UnitCell( as::Vector{Vector{Float64}} , localDim::Int64, rank::Int64)
 			return new{2}( as , Vector{Float64}[] , Bond{2}[] , Vector{Float64}[] , Matrix{ComplexF64}[] , localDim , ones(ComplexF64, length(as)) )
 		end
 	
-		function UnitCell( as::Vector{Vector{Float64}} ; localDim::Int64 =2, rank::Int64 =2)
+		function UnitCell( as::Vector{Vector{Float64}} , localDim::Int64, rank::Int64)
 			@assert length.(as) == repeat([length(as[begin])] , length(as)) "All primitives should have the same dimensions!"
 			return new{rank}( as , Vector{Float64}[] , Bond{rank}[] , Vector{Float64}[] , Matrix{ComplexF64}[] , localDim , ones(ComplexF64, length(as)) )
 		end
