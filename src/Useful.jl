2cfa59967bc468ba70abb564d9672375c9fea576
Andrew Hardy <andrewkoehlerhardy@gmail.com>
2026-03-10 12:06:19 -0400

    fixing docs

--- src/Useful.jl
+++ src/Useful.jl
@@ -129,7 +129,7 @@ Distribution function. `stat`=1 --> Bose-Einstein distribution, and `stat`=-1 --
 ```julia
 DeriDistFunction(E ; T::Float64, mu::Float64=0.0, stat::Int64=-1)
 ```
-derivative of [`dist`](@ref) w.r.t the energy. `stat`=1 --> Bose-Einstein distribution, and `stat`=-1 --> Fermi distribution.
+derivative of [`DistFunction`](@ref) w.r.t the energy. `stat`=1 --> Bose-Einstein distribution, and `stat`=-1 --> Fermi distribution.
 
 """
     function DeriDistFunction(E ; T::Float64, mu::Float64=0.0, stat::Int64=-1)
